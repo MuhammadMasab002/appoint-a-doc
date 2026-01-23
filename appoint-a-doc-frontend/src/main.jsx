90a6410ce71e2cc9ba1f5b22ea2315c95954b349
MuhammadMasab002 <muhammadmasab2002@gmail.com>
2026-01-24 01:56:26 +0500

    feat: setup project

---
+++ appoint-a-doc-frontend/src/main.jsx
@@ -0,0 +1,15 @@
+// import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { store } from "./services/store/store.js";
+import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+);
