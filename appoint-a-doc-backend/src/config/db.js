4ad4aea9453a752d66d99e92b8e1eb921b49d0d6
MuhammadMasab002 <muhammadmasab2002@gmail.com>
2026-02-25 22:31:51 +0500

    stop tracking admin env file

--- appoint-a-doc-backend/src/config/db.js
+++ appoint-a-doc-backend/src/config/db.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
+  const { DB_URI, DB_NAME } = process.env;
   try {
     // Connect to MongoDB and connect to the database
-    await mongoose.connect(`${process.env.DB_URI}/${process.env.DB_NAME}`);
+    await mongoose.connect(`${DB_URI}/${DB_NAME}`);
     console.log("MongoDB connected");
   } catch (error) {
     console.log("MongoDB connection failed:", error);
